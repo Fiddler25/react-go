b2960ef669d3c5e1b3e4c1d7984ccc47bc7a39a1
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-02-27 17:02:46 +0900

    routing設定

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,8 +1,23 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"react-go/backend/controllers"
+)
 
 func Init() {
 	router := gin.Default()
+	endpoints(router)
+
 	router.Run(":3000")
+}
+
+func endpoints(r *gin.Engine) {
+	api := r.Group("/api")
+	{
+		v1api := api.Group("/v1")
+		{
+			v1api.POST("/signup", controllers.Signup)
+		}
+	}
 }
