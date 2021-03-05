5233a5c27ef31bffefc0a683de933a0d69d5f6ce
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-03-06 07:00:28 +0900

    Password必須設定と最小文字数設定を追加

--- backend/models/user.go
+++ backend/models/user.go
@@ -3,5 +3,5 @@ package models
 type User struct {
 	ID       int    `json:"id"`
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password"`
+	Password string `json:"password" binding:"required,min=8"`
 }
