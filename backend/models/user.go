dfa81f440784a40a527739ffdb05ceea1c52bd7e
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-03-06 19:08:34 +0900

    Userモデルから`json:"id"`を削除

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID       int    `json:"id"`
+	ID       int
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8"`
 }
