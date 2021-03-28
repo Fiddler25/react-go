8070f3fdc42f370606ab91dab9b409ad1ee0cb97
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-03-28 15:52:55 +0900

    NewUserメソッド実装

--- backend/application/service/authentication.go
+++ backend/application/service/authentication.go
@@ -3,7 +3,14 @@ package service
 import (
 	"context"
 	"react-go/backend/domain/model"
+	"react-go/backend/domain/repository"
 )
 
 func SignUpService(ctx context.Context, params *model.User) (user *model.User, err error) {
+	user, err = repository.NewUser(params.Email, params.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
--- backend/domain/repository/user.go
+++ backend/domain/repository/user.go
@@ -1,8 +1,18 @@
 package repository
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"react-go/backend/domain/model"
 )
 
 func NewUser(email, password string) (*model.User, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.User{
+		Email:    email,
+		Password: string(hashed),
+	}, nil
 }
