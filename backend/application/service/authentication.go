4375e833f03e3116309dd43a09d3c62a905acbac
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-03-28 17:54:36 +0900

    service層のロジックをmodelに移行

--- backend/application/service/authentication.go
+++ backend/application/service/authentication.go
@@ -2,42 +2,20 @@ package service
 
 import (
 	"context"
-	"log"
-	"react-go/backend/infra/db"
 	"react-go/backend/domain/model"
-	"react-go/backend/domain/repository"
 )
 
 func SignUpService(ctx context.Context, params *model.User) (user *model.User, err error) {
-	user, err = repository.NewUser(params.Email, params.Password)
+	user, err = model.NewUser(params.Email, params.Password)
 	if err != nil {
 		return nil, err
 	}
 
-	id, err := createUser(ctx, user)
+	id, err := model.CreateUser(ctx, user)
 	if err != nil {
 		return nil, err
 	}
 	user.ID = id
 
 	return user, nil
-}
-
-func createUser(ctx context.Context, user *model.User) (int, error) {
-	db := db.DBConnector()
-	defer db.Close()
-
-	sql := "INSERT INTO users (Email, Password) VALUES (?, ?)"
-	stmt, err := db.PrepareContext(ctx, sql)
-	defer func() {
-		err = stmt.Close()
-		if err != nil {
-			log.Println(err)
-		}
-	}()
-
-	result, err := stmt.ExecContext(ctx, user.Email, user.Password)
-	id, err := result.LastInsertId()
-
-	return int(id), nil
 }
--- backend/domain/model/user.go
+++ backend/domain/model/user.go
@@ -1,7 +1,45 @@
 package model
 
+import (
+	"context"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"react-go/backend/infra/db"
+)
+
 type User struct {
 	ID       int
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=8"`
+}
+
+func NewUser(email, password string) (*User, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		Email:    email,
+		Password: string(hashed),
+	}, nil
+}
+
+func CreateUser(ctx context.Context, user *User) (int, error) {
+	db := db.DBConnector()
+	defer db.Close()
+
+	sql := "INSERT INTO users (Email, Password) VALUES (?, ?)"
+	stmt, err := db.PrepareContext(ctx, sql)
+	defer func() {
+		err = stmt.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	result, err := stmt.ExecContext(ctx, user.Email, user.Password)
+	id, err := result.LastInsertId()
+
+	return int(id), nil
 }
