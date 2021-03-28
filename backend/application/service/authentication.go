5e9f4d80d830b54ad49757eb593579ea8a327676
AtsushiKanayama <rager.mka.ti@icloud.com>
2021-03-28 15:54:08 +0900

    createUserメソッド実装

--- backend/application/service/authentication.go
+++ backend/application/service/authentication.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"log"
+	"react-go/backend/config/db"
 	"react-go/backend/domain/model"
 	"react-go/backend/domain/repository"
 )
@@ -12,5 +14,30 @@ func SignUpService(ctx context.Context, params *model.User) (user *model.User, e
 		return nil, err
 	}
 
+	id, err := createUser(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = id
+
 	return user, nil
+}
+
+func createUser(ctx context.Context, user *model.User) (int, error) {
+	db := db.DBConnector()
+	defer db.Close()
+
+	sql := "INSERT INTO users (Email, Password) VALUES (?, ?)"
+	stmt, err := db.PrepareContext(ctx, sql)
+	defer func() {
+		err = stmt.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	result, err := stmt.ExecContext(ctx, user.Email, user.Password)
+	id, err := result.LastInsertId()
+
+	return int(id), nil
 }
